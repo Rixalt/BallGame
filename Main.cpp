03b82128f53b2a8261c469a59d8b7ca07704f611
Anatolii <Rixalts@gmail.com>
2024-02-26 11:09:07 +0200

    Додано вікно

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,13 @@
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/VideoMode.hpp>
 #include <iostream>
+#include <stdio.h>
+#include <SFML/Graphics.hpp>
+
+const int WIDTH = 1500;
+const int HEIGHT = 1500;
+
+sf::RenderWindow window(sf::VideoMode(WIDTH,HEIGHT), "BallGame");
 
 using namespace std;
 
